e67c47a55167d5c503fe9d3a7f76523256aa4032
Mario Pece <mariopece@icloud.com>
2020-11-29 21:35:24 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,59 +1,31 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
-const mime = require('mime');
-const chatServer = require('./lib/chat_server');
-const cache = {};
+var express = require('express')
+var fs = require('fs')
+var app = express()
 
-function send404(response) {
-    response.writeHead(404, {'Content-Type': 'text/plain'});
-    response.write('Error 404: resource not found.');
-    response.end();
-}
+app.use(express.bodyParser())
 
-function sendFile(response, filePath, fileContents) {
-    response.writeHead(
-        200,
-        {"content-type": mime.lookup(path.basename(filePath))}
-    );
-    response.end(fileContents);
-}
+app.get('/', function(request, response) {
+  console.log('GET /')
+  var html = `
+    <html>
+        <body>
+            <form method="post" action="http://localhost:3000">Name: 
+                <input type="text" name="name" />
+                <input type="submit" value="Submit" />
+            </form>
+        </body>
+    </html>`
+  response.writeHead(200, {'Content-Type': 'text/html'})
+  response.end(html)
+})
 
-function serveStatic(response, cache, absPath) {
-    if (cache[absPath]) {
-        sendFile(response, absPath, cache[absPath]);
-    } else {
-        fs.exists(absPath, function(exists) {
-            if (exists) {
-                fs.readFile(absPath, function(err, data) {
-                    if (err) {
-                        send404(response);
-                    } else {
-                        cache[absPath] = data;
-                        sendFile(response, absPath, data);
-                    }
-                });
-            } else {
-                send404(response);
-            }
-        });
-    }
-}
+app.post('/', function(request, response) {
+  console.log('POST /')
+  console.dir(request.body)
+  response.writeHead(200, {'Content-Type': 'text/html'})
+  response.end('thanks')
+})
 
-const server = http.createServer(function(request, response) {
-    let filePath = false;
-    if (request.url === '/') {
-        filePath = 'public/index.html';
-    } else {
-        filePath = 'public' + request.url;
-    }
-
-    const absPath = './' + filePath;
-    serveStatic(response, cache, absPath);
-});
-
-server.listen(process.env.PORT, () => {
-    console.log(`Server listening on port ${3000}`);
-});
-
-chatServer.listen(server);
+port = process.env.PORT
+app.listen(port)
+console.log(`Listening at http://localhost:${port}`)
